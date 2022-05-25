fa9101f92f3dd9842aa750b713f967a630e2d33e
Monica-Claire <monicaclairewallace@gmail.com>
2022-05-25 07:58:21 -0400

    broken

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const profileDataArgs = process.argv.slice(2);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const generatePage = (userName, githubName) => `Name: ${userName}, Gitbub: ${githubName}`;
+console.log(generatePage(name, github));
 
-  console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
